9d8a8ff042c23eec54966e6d3de7b43f9c1225ce
Wesley Silva <wesleylaudelino@outlook.com>
2017-09-29 20:52:36 -0400

    Adicionar arquivos de projeto.

---
+++ Projeto01/Controllers/CategoriasController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using Projeto01.Models;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace Projeto01.Controllers
+{
+    public class CategoriasController : Controller
+    {
+        private static IList<Categoria> categorias =
+            new List<Categoria>()
+            {
+                new Categoria()
+                {
+                    CategoriaID = 1,
+                    nome = "Notebooks"
+                },
+                new Categoria()
+                {
+                    CategoriaID = 2,
+                    nome = "Monitores"
+                },
+                new Categoria()
+                {
+                    CategoriaID = 3,
+                    nome = "Impressoras"
+                },
+                new Categoria()
+                {
+                    CategoriaID = 4,
+                    nome="Mouses"
+
+                },
+                new Categoria()
+                {
+                    CategoriaID = 5,
+                    nome = "Desktops"
+                }
+
+            };
+
+
+        // GET: Categorias
+        public ActionResult Index()
+        {
+            return View(categorias);
+        }
+        // GET: Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Categoria categoria)
+        {
+            categorias.Add(categoria);
+            categoria.CategoriaID = categorias.Select(m => m.CategoriaID).Max() + 1;
+            return RedirectToAction("Index");
+        }
+    }
+}
